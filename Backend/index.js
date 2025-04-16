7dbd513fda57b587778c7c723e77c7e874d9386f
Pavithran-Francis <pavithranfrancis123@gmail.com>
2025-04-16 16:30:00 +0530

    Made changes according to railway deployment

--- Backend/index.js
+++ Backend/index.js
@@ -139,7 +139,13 @@ app.delete('/api/rooms/:id', (req, res) => {
   }
 })
 
-const PORT = process.env.PORT || 3001
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+// const PORT = process.env.PORT || 3001
+// server.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
+
+const cors = require('cors');
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}))
